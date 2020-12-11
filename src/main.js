f3d767e35ad3843077f5ec334c302fb235edad0f
majiabin <majiabin@jianke.com>
2020-12-11 19:00:22 +0800

    🦄refactor(main.js): 多余代码

--- src/main.js
+++ src/main.js
@@ -3,10 +3,6 @@ import App from './App.vue'
 import routes from './routes'
 import './index.css'
 
-routes.forEach((route) => {
-  route.props = (r) => ({ ...(r.meta.state || {}), ...(r.props || {}) })
-})
-
 export default viteSSR(
   App,
   { routes },
@@ -14,8 +10,6 @@ export default viteSSR(
     // The 'request' is the original server request (undefined in browser).
     // The 'initialState' is only available in the browser and can be used to
     // pass it to Vuex, for example, if you prefer to rely on stores rather than Page props.
-    console.log('viteSSR: ================');
-    console.log(isClient);
     router.beforeEach(async (to, from, next) => {
       next()
     })
